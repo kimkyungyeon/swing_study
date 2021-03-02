1124f9dab8502bb38dd4f0c50ed7c2e3f29f2955
kykim <kymonk1@gmail.com>
2021-03-02 17:10:01 +0900

    Jcomponent

--- src/main/java/swing_study/component/FrameComponentEx.java
+++ src/main/java/swing_study/component/FrameComponentEx.java
@@ -20,6 +20,7 @@ public class FrameComponentEx extends JFrame implements ActionListener {
 	private JButton btn2;
 	private JButton btn3;
 	private JButton btn1;
+	private JButton btn_1;
 
 	/**
 	 * Launch the application.
@@ -53,10 +54,7 @@ public class FrameComponentEx extends JFrame implements ActionListener {
 		setContentPane(contentPane);
 		contentPane.setLayout(new GridLayout(0, 1, 0, 0));
 
-		btn1 = new JButton("MagentaYellow Button");
-		btn1.setBackground(new Color(255, 255, 0));
-		btn1.setFont(new Font("Arial", Font.ITALIC, 20));
-		btn1.setForeground(Color.MAGENTA);
+		btn1 = createButton();
 		contentPane.add(btn1);
 
 		btn2 = new JButton("Disabled Button");
@@ -70,7 +68,19 @@ public class FrameComponentEx extends JFrame implements ActionListener {
 		contentPane.add(btn3);
 	}
 
+	public JButton createButton() {
+		btn_1 = new JButton("MagentaYellow Button");
+		btn_1.addActionListener(this);
+		btn_1.setBackground(new Color(255, 255, 0));
+		btn_1.setFont(new Font("Arial", Font.ITALIC, 20));
+		btn_1.setForeground(Color.MAGENTA);
+		return btn_1;
+	}
+
 	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btn_1) {
+			actionPerformedBtn(e);
+		}
 		if (e.getSource() == btn2) {
 			actionPerformedBtn2(e);
 		}
@@ -93,14 +103,29 @@ public class FrameComponentEx extends JFrame implements ActionListener {
 
 	protected void actionPerformedBtn2(ActionEvent e) {
 		if (btn1 != null) {
+
 			contentPane.remove(btn1);
+			btn1 = null;
 			revalidate();
 		} else {
-			if (btn1.isVisible()) {
-				btn1.setVisible(false);
-			} else {
-				btn1.setVisible(true);
-			}
+
+			btn1 = createButton();
+			contentPane.add(btn1, 0);
+			revalidate();
+
+//			if (btn1.isVisible()) {
+//				btn1.setVisible(false);
+//			} else {
+//				btn1.setVisible(true);
+//			}
+		}
+	}
+
+	protected void actionPerformedBtn(ActionEvent e) {
+		if (btn1.isVisible()) {
+			btn1.setVisible(false);
+		} else {
+			btn1.setVisible(true);
 		}
 	}
 }
